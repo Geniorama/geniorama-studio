cb638595a4f07480d1247257309e7d1c4be44340
angelpublicista <angelpublicista.1@gmail.com>
2025-02-13 17:46:31 -0500

    first commit

---
+++ schemaTypes/projectType.ts
@@ -0,0 +1,57 @@
+import {defineField, defineType} from 'sanity'
+
+export const projectType = defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+        name: 'language',
+        type: 'string',
+        validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'description',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+        name: 'featured',
+        type: 'boolean',
+        initialValue: false
+    }),
+    defineField({
+      name: 'shortDescription',
+      type: 'text',
+    }),
+    defineField({
+        name: 'clientName',
+        type: 'string',
+    }),
+    defineField({
+        name: 'projectLink',
+        type: 'url',
+    }),
+  ],
+})
